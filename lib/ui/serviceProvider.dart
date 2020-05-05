dcc4397994636d2101c9d711735aaf2c81e48ac5
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-05-05 07:51:13 +0530

    modify service provider

---
+++ lib/ui/serviceProvider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ServiceProvider extends StatelessWidget {
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Service Partners',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
